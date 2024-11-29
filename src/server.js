ea06e5f62529bce70e1bf08b628cace6ba0e7b35
04_burapa(blue/บลู) <burapa.pratumchai@gmail.com>
2024-11-29 15:13:31 +0700

    ok

---
+++ src/server.js
@@ -0,0 +1,67 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./config/mongodb.js";
+import connectCloudinary from "./config/cloudinary.js";
+// Custom routes
+import userRouter from "./routes/userRoute.js";
+import productRouter from "./routes/productRoute.js";
+import cartRouter from "./routes/cartRoute.js";
+import orderRouter from "./routes/orderRoute.js";
+
+// App Config
+const app = express();
+const port = process.env.PORT || 4000;
+
+// Service connections
+connectDB();
+connectCloudinary();
+
+const allowedOrigins = [
+    "https://artnakkk-frontend-admin.vercel.app",
+    "http://localhost:5175",
+    "http://localhost:5176",
+    process.env.FRONTEND_URL,
+];
+
+// middlewares
+app.use(express.json());
+// Configure CORS
+app.use(
+    cors({
+        origin: (origin, callback) => {
+            if (!origin) return callback(null, true);
+            if (allowedOrigins.includes(origin)) {
+                callback(null, true);
+            } else {
+                callback(new Error("Not allowed by CORS"));
+            }
+        },
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true,
+    })
+);
+
+// api endpoints
+app.use("/api/user", userRouter);
+app.use("/api/product", productRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
+
+app.get("/", (req, res) => {
+  res.send("API Working");
+});
+
+// เพิ่ม error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({
+        status: 'error',
+        message: 'Something went wrong!'
+    });
+});
+
+// Start server
+app.listen(port, () =>
+  console.log("Server started on PORT : " + port + " 🌏 🙌")
+);
