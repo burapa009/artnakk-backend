c19bd49d072bfe1d63d983d5b96087d7fb3662e9
04_burapa(blue/บลู) <burapa.pratumchai@gmail.com>
2024-11-29 15:20:43 +0700

    routes

--- src/routes/orderRoute.js
+++ src/routes/orderRoute.js
@@ -5,23 +5,16 @@ import {
   userOrders,
   updateStatus,
   verifyStripe,
-} from "../../mern-notes-app-backend/controllers/orderController.js";
+} from "../controllers/orderController.js";
 import adminAuth from "../middleware/adminAuth.js";
 import authUser from "../middleware/auth.js";
 
 const orderRouter = express.Router();
 
-// Admin Features
 orderRouter.post("/list", adminAuth, allOrders);
 orderRouter.post("/status", adminAuth, updateStatus);
-
-// Payment Features
 orderRouter.post("/stripe", authUser, placeOrderStripe);
-
-// User Feature
 orderRouter.post("/userorders", authUser, userOrders);
-
-// verify payment
 orderRouter.post("/verifyStripe", authUser, verifyStripe);
 
-export default orderRouter;
+export default orderRouter; 
